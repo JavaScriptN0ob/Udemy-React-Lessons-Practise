504e72e1834e55d3c340545006e9331796231da4
JavaScriptN0ob <wenpeijs@gmail.com>
2020-08-20 19:02:41 +1000

    Updates

---
+++ precache-manifest.e12fb8f322a4ea9d251e3504c9d081a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40580e5b5c82caf8359babd25c9ce201",
+    "url": "/shpping-webapp-wayne/index.html"
+  },
+  {
+    "revision": "2f27f9a5f4049ce436ee",
+    "url": "/shpping-webapp-wayne/static/css/main.20d529ed.chunk.css"
+  },
+  {
+    "revision": "0a129d15598a436fa580",
+    "url": "/shpping-webapp-wayne/static/js/2.0aa82808.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shpping-webapp-wayne/static/js/2.0aa82808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f27f9a5f4049ce436ee",
+    "url": "/shpping-webapp-wayne/static/js/main.5145a83a.chunk.js"
+  },
+  {
+    "revision": "28b4e73c4c2d5c6156ed",
+    "url": "/shpping-webapp-wayne/static/js/runtime-main.4fee8780.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shpping-webapp-wayne/static/media/crown.ed28accb.svg"
+  }
+]);
